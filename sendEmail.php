aa65609b863bc1b6b3d004c3dc5611fd0efb05a4
joshuamakula <makulajosh@gmail.com>
2021-05-07 17:28:22 +0300

    SMTP setting

--- sendEmail.php
+++ sendEmail.php
@@ -1,4 +1,31 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 
+    if(isset($_POST['fname']) && isset($_POST['lname'])){
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $email = $_POST['email'];
+        $subject = $_POST['subject'];
+        $message = $_POST['message'];
+
+        //Import PHPMailer classes into the global namespace
+        require_once "PHPMailer/PHPMailer/PHPMailer";
+        require_once "PHPMailer/SMTP.php";
+        require_once "PHPMailer/Exception";
+
+        //Instantiation
+        $mail = new PHPMailer();
+
+        // smtp settings
+        $mail->isSMTP();
+        $mail->Host = "smtp.gmail.com";
+        $mail->SMTPAuth = true;
+        $mail->Username = "ssembatyafred444@gmail.com";
+        $mail->Password = 'SCHOOL50';
+        $mail->Port="456";
+        $mail->SMTPSecure="ssl"
+
+    }
+
+
 ?>
