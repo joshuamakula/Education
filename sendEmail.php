e850c15c5a64a35d5a223e1ad8a112a058236e58
joshuamakula <makulajosh@gmail.com>
2021-05-11 17:01:27 +0300

    change the email function

--- sendEmail.php
+++ sendEmail.php
@@ -3,99 +3,51 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require_once __DIR__ . '/vendor/phpmailer/src/Exception.php';
-require_once __DIR__ . '/vendor/phpmailer/src/PHPMailer.php';
-require_once __DIR__ . '/vendor/phpmailer/src/SMTP.php';
+    if(isset($_POST['fname']) && isset($_POST['email'])){
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $email = $_POST['email'];
+        $subject = $_POST['subject'];
+        $message = $_POST['message'];
 
-// passing true in constructor enables exceptions in PHPMailer
-$mail = new PHPMailer(true);
+        //Import PHPMailer classes into the global namespace
+        require_once "PHPMailer/PHPMailer.php";
+        require_once "PHPMailer/SMTP.php";
+        require_once "PHPMailer/Exception.php";
 
-try {
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
-    $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
+        //Instantiation
+        $mail = new PHPMailer();
 
-    // Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER; // for detailed debug output
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port = 587;
+        // smtp settings
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+        $mail->isSMTP();
+        $mail->Host = "smtp.gmail.com";
+        $mail->SMTPAuth = true;
+        $mail->Username = "ssembatyafred444@gmail.com";
+        $mail->Password = 'SCHOOL50';
+        $mail->Port="465";
+        $mail->SMTPSecure="ssl"
 
-    $mail->Username = 'makulajosh@gmail.com'; // YOUR gmail email
-    $mail->Password = 'makukula'; // YOUR gmail password
+        // Recipients email settings
+        $mail->isHTML(true);
+        $mail->setFrom($email, $fname);
+        $mail->addAddress("makulajosh@gmail.com"); //Set who the message is to be sent to
+        $mail->Subject = ("$email ($subject)");
+        $mail->Body = $message;
 
-    // Sender and recipient settings
-    $mail->setFrom($email, $fname);
-    $mail->addAddress('makulajosh@gmail.com', 'Receiver Name');
-    $mail->addReplyTo($email, $fname); // to set the reply to
+        if($mail->send()){
+            $status = "success";
+            $response = "Email is sent";
+        }
+        else
+        {
+            $status = "failed";
+            $response = "Something is wrong: <br>" .$mail->ErrorInfo;
+        }
 
-    // Setting the email content
-    $mail->IsHTML(true);
-    $mail->Subject = $subject;
-    $mail->Body = $message;
-    // $mail->AltBody = 'Plain text message body for non-HTML email client. Gmail SMTP email body.';
+        exit(json_encode(array("status" => $status, "response" => $response)));
 
-    $mail->send();
-    echo "Email message sent.";
-} catch (Exception $e) {
-    echo "Error in sending email. Mailer Error: {$mail->ErrorInfo}";
-}
-
-
-
-// use PHPMailer\PHPMailer\PHPMailer;
-// use PHPMailer\PHPMailer\SMTP;
-// use PHPMailer\PHPMailer\Exception;
-
-//     if(isset($_POST['fname']) && isset($_POST['email'])){
-//         $fname = $_POST['fname'];
-//         $lname = $_POST['lname'];
-//         $email = $_POST['email'];
-//         $subject = $_POST['subject'];
-//         $message = $_POST['message'];
-
-//         //Import PHPMailer classes into the global namespace
-//         require_once "PHPMailer/PHPMailer.php";
-//         require_once "PHPMailer/SMTP.php";
-//         require_once "PHPMailer/Exception.php";
-
-//         //Instantiation
-//         $mail = new PHPMailer();
-
-//         // smtp settings
-//         $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-//         $mail->isSMTP();
-//         $mail->Host = "smtp.gmail.com";
-//         $mail->SMTPAuth = true;
-//         $mail->Username = "ssembatyafred444@gmail.com";
-//         $mail->Password = 'SCHOOL50';
-//         $mail->Port="465";
-//         $mail->SMTPSecure="ssl"
-
-//         // Recipients email settings
-//         $mail->isHTML(true);
-//         $mail->setFrom($email, $fname);
-//         $mail->addAddress("makulajosh@gmail.com"); //Set who the message is to be sent to
-//         $mail->Subject = ("$email ($subject)");
-//         $mail->Body = $message;
-
-//         if($mail->send()){
-//             $status = "success";
-//             $response = "Email is sent";
-//         }
-//         else
-//         {
-//             $status = "failed";
-//             $response = "Something is wrong: <br>" .$mail->ErrorInfo;
-//         }
-
-//         exit(json_encode(array("status" => $status, "response" => $response)));
-
-//     }
+    }
 
 
 ?> 
