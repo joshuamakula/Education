33534cd8d58c06aabfdb856fa00555f9a4ad32ba
joshuamakula <makulajosh@gmail.com>
2021-05-16 21:52:12 +0300

    Show success message

--- PHP/contact-form-handler.php
+++ PHP/contact-form-handler.php
@@ -24,7 +24,36 @@ $email_address))
     $errors .= "\n Error: Invalid email address";
 }
 
+// Email form data
+if( empty($errors))
 
+{
 
+$to = $myemail;
+
+$email_subject = $subject;
+
+$email_body = "You have received a new message. ".
+
+    " Here are the details:\n Name: $fname $lname\n ".
+
+    "Email: $email_address\n Message \n $message";
+
+$headers = "From: $myemail\n";
+
+$headers .= "Reply-To: $email_address";
+
+mail($to,$email_subject,$email_body,$headers);
+
+//redirect to the 'thank you' page
+
+// header('Location: ../contact-form-thank-you.html');
+
+header('location: ../contact.php?status=success');
+if( $_GET['status'] == 'success'):
+    echo 'Your message has been sent successfully';
+endif;
+
+}
 
 ?>
